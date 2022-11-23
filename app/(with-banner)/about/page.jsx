2d966ed381c32622c08da1be2d1f7af3515869ec
THE-FABI7 <fabianguancha13@gmail.com>
2022-11-23 11:05:32 -0500

    Actualizacion final2

--- app/(with-banner)/about/page.jsx
+++ app/(with-banner)/about/page.jsx
@@ -1,15 +1,48 @@
-import Image from "next/image"
+import Image from "next/image";
 /**
  * Esta función devuelve un elemento React que muestra un encabezado h1 con el texto 'Esta es la página
  * de Acerca de'.
  * @returns Un elemento Reaccionar.
  */
 export default function AboutPage() {
-  return(
+  return (
     <section>
-           <h1>Esta es la pagina de About</h1>;
+      <h1>Esta es la pagina de About</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni ab
+        voluptas quibusdam, debitis nulla, ex veniam repellendus ipsa natus
+        dolores repellat tenetur corrupti sapiente quam libero itaque, nihil
+        provident perferendis?
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia mollitia
+        dolores ea magni expedita ducimus! Repellendus, at cupiditate iure nisi
+        deleniti autem cumque eum architecto commodi sunt? Exercitationem, eum
+        mollitia.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis quod
+        officiis, voluptas architecto nisi consectetur repudiandae ratione saepe
+        molestiae. Alias corporis reiciendis veniam atque, odit necessitatibus
+        omnis possimus. Mollitia, nostrum. Lorem ipsum dolor sit amet
+        consectetur, adipisicing elit. Voluptatem saepe qui laborum natus? Eum
+        unde vel saepe, neque ducimus eaque illo error quam eos et dolorum,
+        dolores quo! Saepe, velit?. Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Sint quaerat, dolor impedit optio fugiat perferendis
+        architecto dignissimos quisquam quam consectetur ipsa eius repellat
+        sunt! Adipisci fugiat assumenda incidunt autem ex. Lorem ipsum dolor
+        sit, amet consectetur adipisicing elit. Delectus voluptates, quas ullam
+        facilis repellendus praesentium inventore asperiores omnis iusto ducimus
+        enim repudiandae doloremque. Alias exercitationem sunt enim facere
+        mollitia maiores?. Lorem, ipsum dolor sit amet consectetur adipisicing
+        elit. Tempora nulla aspernatur quasi ipsam repellat ex. Maiores est
+        excepturi dolorem quidem voluptate temporibus, inventore dolore?
+        Consequatur reiciendis provident consectetur modi omnis?. Lorem ipsum
+        dolor sit amet consectetur adipisicing elit. Dolore optio eius at
+        reprehenderit laudantium commodi nostrum dolorum officia maxime
+        repellendus. Minus sed, doloremque alias enim quasi consequuntur
+        possimus! Nisi, minus.
+      </p>
     </section>
-  )
-  
- 
+  );
 }
--- app/page.jsx
+++ app/page.jsx
@@ -8,6 +8,17 @@ export default function HomePage() {
   return (
     <section>
       <h1>Bienvenidos a FabiApp</h1>
+      <p>
+        Fabi app esta basada en el video del siguiente Tutorial:{" "}
+        <a
+          style={{ color: "yellow" }}
+          href="https://www.youtube.com/watch?v=pFT8wD2uRSE"
+        >
+          next js
+        </a>{" "}
+        conservando sus estilos y formatos
+      </p>
+      <p>Cabe destacar que los post funcionan con conexion a internet :)</p>
     </section>
   );
 }
