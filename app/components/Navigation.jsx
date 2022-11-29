9f27f99d4a149001247ba8df81271a428519e74b
THE-FABI7 <fabianguancha13@gmail.com>
2022-11-29 15:28:24 -0500

    documentacion

--- app/components/Navigation.jsx
+++ app/components/Navigation.jsx
@@ -14,7 +14,7 @@ const link = [
   {
     label: "Posts",
     route: "/posts",
-  },
+  }
 ];
 
 /**
--- app/font.js
+++ app/font.js
@@ -1,5 +1,6 @@
 import { Space_Grotesk as SpaceGrotesk} from "@next/font/google";
-
+/* Importar la fuente de la API de fuentes de Google y configurar el peso y el subconjunto de la
+fuente. */
  export const font = SpaceGrotesk({
     weight: ['400', '700'],
     subsets: ['latin'],
